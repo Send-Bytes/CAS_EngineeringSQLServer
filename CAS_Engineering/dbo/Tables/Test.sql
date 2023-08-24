cecc1500def71d24794d1570f897c8bac22b49c5
caityD <142865980+caityD@users.noreply.github.com>
2023-08-24 22:46:07 +0200

    The actual databases, forgot to publish the first time

---
+++ CAS_Engineering/dbo/Tables/Test.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Test] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (50) NULL,
+    [Age]  INT           NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
