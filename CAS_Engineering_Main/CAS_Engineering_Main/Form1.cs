a617a5f747ca392b4c1dbc1af7251291b4092d41
Send-Bytes <areynardt@outlook.com>
2023-08-26 19:54:19 +0200

    Initial Changes

--- CAS_Engineering_Main/CAS_Engineering_Main/Form1.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Form1.cs
@@ -41,12 +41,23 @@ namespace CAS_Engineering_Main
                 conn.Open();
                 MessageBox.Show("Connected");
 
+                string query = "INSERT INTO YourTable (User_ID) VALUES (@id)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", 1);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data inserted");
+
             }
             catch (SqlException error)
             {
                 MessageBox.Show("Error connecting to database" + error.ToString());
             }
             conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
