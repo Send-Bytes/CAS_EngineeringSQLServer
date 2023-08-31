a3079ba2ca48b222d17a6c029e0a53e100b85d66
Send-Bytes <areynardt@outlook.com>
2023-08-31 16:11:32 +0200

    Dashboard Changes

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDashPM.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDashPM.Designer.cs
@@ -33,7 +33,6 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
@@ -41,34 +40,51 @@
             this.label11 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.label16 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlAdmin = new System.Windows.Forms.Panel();
+            this.pnlAdminV = new System.Windows.Forms.Panel();
+            this.pnlProject = new System.Windows.Forms.Panel();
+            this.pnlProjectV = new System.Windows.Forms.Panel();
+            this.pnlEquip = new System.Windows.Forms.Panel();
+            this.pnlEquipV = new System.Windows.Forms.Panel();
+            this.label18 = new System.Windows.Forms.Label();
+            this.pnlPay = new System.Windows.Forms.Panel();
+            this.pnlPayV = new System.Windows.Forms.Panel();
+            this.label19 = new System.Windows.Forms.Label();
+            this.pnlReports = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.pnlAdmin.SuspendLayout();
+            this.pnlProject.SuspendLayout();
+            this.pnlEquip.SuspendLayout();
+            this.pnlPay.SuspendLayout();
+            this.pnlReports.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(44, 26);
+            this.label1.Location = new System.Drawing.Point(418, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.Size = new System.Drawing.Size(70, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Project Manager";
+            this.label1.Text = "Project Panel";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(563, 26);
+            this.label2.Location = new System.Drawing.Point(390, 46);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(97, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Administrators";
+            this.label2.Text = "Administrator Panel";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(566, 65);
+            this.label3.Location = new System.Drawing.Point(12, 76);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(128, 13);
             this.label3.TabIndex = 2;
@@ -77,7 +93,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(566, 100);
+            this.label4.Location = new System.Drawing.Point(351, 76);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(136, 13);
             this.label4.TabIndex = 3;
@@ -86,25 +102,16 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(569, 139);
+            this.label5.Location = new System.Drawing.Point(174, 76);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(159, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Update Projects - frmProjUpdate";
             // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(47, 65);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(83, 13);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Update Projects";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(569, 184);
+            this.label7.Location = new System.Drawing.Point(12, 75);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(148, 13);
             this.label7.TabIndex = 6;
@@ -113,7 +120,7 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(581, 220);
+            this.label8.Location = new System.Drawing.Point(12, 77);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(114, 13);
             this.label8.TabIndex = 7;
@@ -122,7 +129,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(572, 256);
+            this.label9.Location = new System.Drawing.Point(204, 75);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(159, 13);
             this.label9.TabIndex = 8;
@@ -131,7 +138,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(572, 297);
+            this.label10.Location = new System.Drawing.Point(325, 72);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(180, 13);
             this.label10.TabIndex = 9;
@@ -140,7 +147,7 @@
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(540, 329);
+            this.label11.Location = new System.Drawing.Point(635, 77);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(212, 13);
             this.label11.TabIndex = 10;
@@ -149,7 +156,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(540, 364);
+            this.label12.Location = new System.Drawing.Point(390, 75);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(213, 13);
             this.label12.TabIndex = 11;
@@ -158,25 +165,16 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(540, 396);
+            this.label13.Location = new System.Drawing.Point(400, 56);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(87, 13);
             this.label13.TabIndex = 12;
             this.label13.Text = "Request Reports";
             // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(60, 100);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(110, 13);
-            this.label14.TabIndex = 13;
-            this.label14.Text = "Request some reports";
-            // 
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(540, 421);
+            this.label15.Location = new System.Drawing.Point(224, 77);
             this.label15.Name = "label15";
             this.label15.Size = new System.Drawing.Size(130, 13);
             this.label15.TabIndex = 14;
@@ -185,7 +183,7 @@
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(540, 444);
+            this.label16.Location = new System.Drawing.Point(635, 75);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(151, 13);
             this.label16.TabIndex = 15;
@@ -194,38 +192,166 @@
             // label17
             // 
             this.label17.AutoSize = true;
-            this.label17.Location = new System.Drawing.Point(379, 65);
+            this.label17.Location = new System.Drawing.Point(418, 77);
             this.label17.Name = "label17";
             this.label17.Size = new System.Drawing.Size(120, 13);
             this.label17.TabIndex = 16;
             this.label17.Text = "Update User - frmUpUsr";
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Controls.Add(this.pnlAdmin);
+            this.flowLayoutPanel1.Controls.Add(this.pnlAdminV);
+            this.flowLayoutPanel1.Controls.Add(this.pnlProject);
+            this.flowLayoutPanel1.Controls.Add(this.pnlProjectV);
+            this.flowLayoutPanel1.Controls.Add(this.pnlEquip);
+            this.flowLayoutPanel1.Controls.Add(this.pnlEquipV);
+            this.flowLayoutPanel1.Controls.Add(this.pnlPay);
+            this.flowLayoutPanel1.Controls.Add(this.pnlPayV);
+            this.flowLayoutPanel1.Controls.Add(this.pnlReports);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(313, 165);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(918, 526);
+            this.flowLayoutPanel1.TabIndex = 17;
+            // 
+            // pnlAdmin
+            // 
+            this.pnlAdmin.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlAdmin.Controls.Add(this.label8);
+            this.pnlAdmin.Controls.Add(this.label2);
+            this.pnlAdmin.Controls.Add(this.label15);
+            this.pnlAdmin.Controls.Add(this.label11);
+            this.pnlAdmin.Controls.Add(this.label17);
+            this.pnlAdmin.Location = new System.Drawing.Point(3, 3);
+            this.pnlAdmin.Name = "pnlAdmin";
+            this.pnlAdmin.Size = new System.Drawing.Size(880, 100);
+            this.pnlAdmin.TabIndex = 18;
+            this.pnlAdmin.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // pnlAdminV
+            // 
+            this.pnlAdminV.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlAdminV.Location = new System.Drawing.Point(3, 109);
+            this.pnlAdminV.Name = "pnlAdminV";
+            this.pnlAdminV.Size = new System.Drawing.Size(880, 100);
+            this.pnlAdminV.TabIndex = 19;
+            this.pnlAdminV.Visible = false;
+            // 
+            // pnlProject
+            // 
+            this.pnlProject.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlProject.Controls.Add(this.label4);
+            this.pnlProject.Controls.Add(this.label3);
+            this.pnlProject.Controls.Add(this.label5);
+            this.pnlProject.Controls.Add(this.label1);
+            this.pnlProject.Location = new System.Drawing.Point(3, 215);
+            this.pnlProject.Name = "pnlProject";
+            this.pnlProject.Size = new System.Drawing.Size(880, 100);
+            this.pnlProject.TabIndex = 20;
+            this.pnlProject.Click += new System.EventHandler(this.pnlProject_Click);
+            // 
+            // pnlProjectV
+            // 
+            this.pnlProjectV.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlProjectV.Location = new System.Drawing.Point(3, 321);
+            this.pnlProjectV.Name = "pnlProjectV";
+            this.pnlProjectV.Size = new System.Drawing.Size(880, 100);
+            this.pnlProjectV.TabIndex = 21;
+            this.pnlProjectV.Visible = false;
+            // 
+            // pnlEquip
+            // 
+            this.pnlEquip.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlEquip.Controls.Add(this.label18);
+            this.pnlEquip.Controls.Add(this.label7);
+            this.pnlEquip.Controls.Add(this.label16);
+            this.pnlEquip.Controls.Add(this.label9);
+            this.pnlEquip.Controls.Add(this.label12);
+            this.pnlEquip.Location = new System.Drawing.Point(3, 427);
+            this.pnlEquip.Name = "pnlEquip";
+            this.pnlEquip.Size = new System.Drawing.Size(880, 100);
+            this.pnlEquip.TabIndex = 22;
+            this.pnlEquip.Click += new System.EventHandler(this.pnlEquip_Click);
+            // 
+            // pnlEquipV
+            // 
+            this.pnlEquipV.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlEquipV.Location = new System.Drawing.Point(3, 533);
+            this.pnlEquipV.Name = "pnlEquipV";
+            this.pnlEquipV.Size = new System.Drawing.Size(880, 100);
+            this.pnlEquipV.TabIndex = 23;
+            this.pnlEquipV.Visible = false;
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Location = new System.Drawing.Point(418, 32);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(87, 13);
+            this.label18.TabIndex = 18;
+            this.label18.Text = "Equipment Panel";
+            // 
+            // pnlPay
+            // 
+            this.pnlPay.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlPay.Controls.Add(this.label10);
+            this.pnlPay.Controls.Add(this.label19);
+            this.pnlPay.Location = new System.Drawing.Point(3, 639);
+            this.pnlPay.Name = "pnlPay";
+            this.pnlPay.Size = new System.Drawing.Size(880, 100);
+            this.pnlPay.TabIndex = 24;
+            this.pnlPay.Click += new System.EventHandler(this.pnlPay_Click);
+            this.pnlPay.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pnlPayV
+            // 
+            this.pnlPayV.BackColor = System.Drawing.SystemColors.Info;
+            this.pnlPayV.Location = new System.Drawing.Point(3, 745);
+            this.pnlPayV.Name = "pnlPayV";
+            this.pnlPayV.Size = new System.Drawing.Size(880, 100);
+            this.pnlPayV.TabIndex = 25;
+            this.pnlPayV.Visible = false;
+            this.pnlPayV.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(371, 42);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(78, 13);
+            this.label19.TabIndex = 18;
+            this.label19.Text = "Payment Panel";
+            // 
+            // pnlReports
+            // 
+            this.pnlReports.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pnlReports.Controls.Add(this.label13);
+            this.pnlReports.Location = new System.Drawing.Point(3, 851);
+            this.pnlReports.Name = "pnlReports";
+            this.pnlReports.Size = new System.Drawing.Size(880, 100);
+            this.pnlReports.TabIndex = 26;
+            // 
             // frmDashPM
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 466);
-            this.Controls.Add(this.label17);
-            this.Controls.Add(this.label16);
-            this.Controls.Add(this.label15);
-            this.Controls.Add(this.label14);
-            this.Controls.Add(this.label13);
-            this.Controls.Add(this.label12);
-            this.Controls.Add(this.label11);
-            this.Controls.Add(this.label10);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(1522, 888);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Name = "frmDashPM";
             this.Text = "Project Manager DashBoard";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.pnlAdmin.ResumeLayout(false);
+            this.pnlAdmin.PerformLayout();
+            this.pnlProject.ResumeLayout(false);
+            this.pnlProject.PerformLayout();
+            this.pnlEquip.ResumeLayout(false);
+            this.pnlEquip.PerformLayout();
+            this.pnlPay.ResumeLayout(false);
+            this.pnlPay.PerformLayout();
+            this.pnlReports.ResumeLayout(false);
+            this.pnlReports.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -236,7 +362,6 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
@@ -244,9 +369,20 @@
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.Label label12;
         private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel pnlAdmin;
+        private System.Windows.Forms.Panel pnlAdminV;
+        private System.Windows.Forms.Panel pnlProject;
+        private System.Windows.Forms.Panel pnlProjectV;
+        private System.Windows.Forms.Panel pnlEquip;
+        private System.Windows.Forms.Panel pnlEquipV;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Panel pnlPay;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Panel pnlReports;
+        private System.Windows.Forms.Panel pnlPayV;
     }
 }
--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDashPM.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDashPM.cs
@@ -16,5 +16,81 @@ namespace CAS_Engineering_Main.Dash
         {
             InitializeComponent();
         }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            if (pnlAdminV.Visible == false)
+            {
+                pnlAdminV.Visible = true;
+
+                pnlEquipV.Visible = false;
+                pnlPayV.Visible = false;
+                pnlProjectV.Visible = false;
+                
+            }
+            else
+            {
+                pnlAdminV.Visible = false;
+            }
+
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pnlProject_Click(object sender, EventArgs e)
+        {
+            if (pnlProjectV.Visible == false)
+            {
+                pnlProjectV.Visible = true;
+
+                pnlEquipV.Visible = false;
+                pnlPayV.Visible = false;
+                pnlAdminV.Visible = false;
+            }
+            else
+            {
+                pnlProjectV.Visible = false;
+            }
+        }
+
+        private void pnlEquip_Click(object sender, EventArgs e)
+        {
+            if (pnlEquipV.Visible == false)
+            {
+                pnlEquipV.Visible = true;
+
+                pnlAdminV.Visible = false;
+                pnlPayV.Visible = false;
+                pnlProjectV.Visible = false;
+            }
+            else
+            {
+                pnlEquipV.Visible = false;
+            }
+        }
+
+        private void pnlPay_Click(object sender, EventArgs e)
+        {
+            if (pnlPayV.Visible == false)
+            {
+                pnlPayV.Visible = true;
+
+                pnlEquipV.Visible = false;
+                pnlAdminV.Visible = false;
+                pnlProjectV.Visible = false;
+            }
+            else
+            {
+                pnlPayV.Visible = false;
+            }
+        }
     }
 }
