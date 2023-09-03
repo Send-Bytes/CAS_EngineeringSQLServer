b39bd89b000d70d29a1824bbf3ee8f0ac4c4b28a
ZaneKennard <142911294+ZaneKennard@users.noreply.github.com>
2023-09-04 00:37:52 +0200

    delete equipment

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.Designer.cs
@@ -28,7 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.listBoxEquipment = new System.Windows.Forms.ListBox();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // listBoxEquipment
+            // 
+            this.listBoxEquipment.FormattingEnabled = true;
+            this.listBoxEquipment.Location = new System.Drawing.Point(217, 91);
+            this.listBoxEquipment.Name = "listBoxEquipment";
+            this.listBoxEquipment.Size = new System.Drawing.Size(120, 95);
+            this.listBoxEquipment.TabIndex = 0;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(238, 224);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // frmDelEquip
             // 
@@ -36,14 +56,20 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.listBoxEquipment);
             this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmDelEquip";
             this.Text = "frmDelEquip";
+            this.Load += new System.EventHandler(this.frmDelEquip_Load_1);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBoxEquipment;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.cs
@@ -7,14 +7,86 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CAS_Engineering_Main.Dash
 {
     public partial class frmDelEquip : Form
     {
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\KENNARD\Documents\GitHub\CAS_EngineeringSQLServer\CAS_Engineering_Main\CAS_Engineering_Main\dboCASEngineering.mdf;Integrated Security=True";//cone tion string
         public frmDelEquip()
         {
             InitializeComponent();
+        }
+
+        private void PopulateListBox()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT E_ID, E_Description FROM Equipment";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string equipmentDescription = reader["E_Description"].ToString();
+                        listBoxEquipment.Items.Add(equipmentDescription);
+                    }
+
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (listBoxEquipment.SelectedItem != null)
+            {
+                string equipmentDescriptionToDelete = listBoxEquipment.SelectedItem.ToString();
+
+                // confirmation 
+                DialogResult result = MessageBox.Show("Are you sure you want to delete this equipment?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (result == DialogResult.Yes)
+                {
+                    // Perform the deletion 
+                    try
+                    {
+                        using (SqlConnection connection = new SqlConnection(connectionString))
+                        {
+                            connection.Open();
+                            string deleteQuery = "DELETE FROM Equipment WHERE E_Description = @EquipmentDescription";
+                            SqlCommand cmd = new SqlCommand(deleteQuery, connection);
+                            cmd.Parameters.AddWithValue("@EquipmentDescription", equipmentDescriptionToDelete);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Equipment deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                            // Remove the selected item from the ListBox.
+                            listBoxEquipment.Items.Remove(equipmentDescriptionToDelete);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an equipment to delete.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void frmDelEquip_Load_1(object sender, EventArgs e)
+        {
+            PopulateListBox();
         }
     }
 }
