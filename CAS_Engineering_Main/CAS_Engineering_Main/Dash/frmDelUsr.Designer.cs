2773fb97190b0ec588aa4fbef6743f7d23752465
Maderid <142977275+Maderid@users.noreply.github.com>
2023-09-04 00:07:14 +0200

    GUI edited

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelUsr.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelUsr.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmDelUsr));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.txtID = new System.Windows.Forms.TextBox();
@@ -46,9 +47,11 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(30, 25);
+            this.dataGridView1.Location = new System.Drawing.Point(55, 31);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(534, 166);
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(712, 204);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
@@ -57,17 +60,19 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(369, 222);
+            this.label1.Location = new System.Drawing.Point(492, 273);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.Size = new System.Drawing.Size(76, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "User_ID:";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(342, 246);
+            this.txtID.Location = new System.Drawing.Point(456, 303);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(119, 20);
+            this.txtID.Size = new System.Drawing.Size(157, 22);
             this.txtID.TabIndex = 3;
             // 
             // groupBox1
@@ -77,26 +82,30 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(41, 222);
+            this.groupBox1.Location = new System.Drawing.Point(55, 273);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(248, 117);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(331, 144);
             this.groupBox1.TabIndex = 5;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Filter By:";
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(130, 61);
+            this.txtSearch.Location = new System.Drawing.Point(173, 75);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(100, 23);
+            this.txtSearch.Size = new System.Drawing.Size(132, 26);
             this.txtSearch.TabIndex = 3;
             this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // txtSearchN
             // 
-            this.txtSearchN.Location = new System.Drawing.Point(130, 24);
+            this.txtSearchN.Location = new System.Drawing.Point(173, 30);
+            this.txtSearchN.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSearchN.Name = "txtSearchN";
-            this.txtSearchN.Size = new System.Drawing.Size(100, 23);
+            this.txtSearchN.Size = new System.Drawing.Size(132, 26);
             this.txtSearchN.TabIndex = 2;
             this.txtSearchN.TextChanged += new System.EventHandler(this.txtSearchN_TextChanged);
             // 
@@ -104,9 +113,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(27, 61);
+            this.label3.Location = new System.Drawing.Point(36, 75);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(63, 17);
+            this.label3.Size = new System.Drawing.Size(76, 20);
             this.label3.TabIndex = 1;
             this.label3.Text = "User_ID:";
             // 
@@ -114,19 +124,20 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(24, 24);
+            this.label2.Location = new System.Drawing.Point(32, 30);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 17);
+            this.label2.Size = new System.Drawing.Size(97, 20);
             this.label2.TabIndex = 0;
             this.label2.Text = "First Name:";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::CAS_Engineering_Main.Properties.Resources._3;
-            this.pictureBox1.Location = new System.Drawing.Point(-12, -2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-14, -10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(612, 369);
+            this.pictureBox1.Size = new System.Drawing.Size(826, 471);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -135,25 +146,26 @@
             // 
             this.btnDelUser.BackColor = System.Drawing.Color.Goldenrod;
             this.btnDelUser.Font = new System.Drawing.Font("Goudy Old Style", 11.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelUser.Location = new System.Drawing.Point(346, 292);
+            this.btnDelUser.Location = new System.Drawing.Point(461, 359);
+            this.btnDelUser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDelUser.Name = "btnDelUser";
-            this.btnDelUser.Size = new System.Drawing.Size(114, 34);
+            this.btnDelUser.Size = new System.Drawing.Size(152, 42);
             this.btnDelUser.TabIndex = 23;
             this.btnDelUser.Text = "Delete User";
             this.btnDelUser.UseVisualStyleBackColor = false;
             // 
             // frmDelUsr
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnDelUser);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmDelUsr";
             this.Text = "frmDelUsr";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
