1c54f5ced26ab3e642f48b7185aa0fe0243f1345
ZaneKennard <142911294+ZaneKennard@users.noreply.github.com>
2023-09-04 14:09:00 +0200

    design

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmDelEquip.Designer.cs
@@ -28,48 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBoxEquipment = new System.Windows.Forms.ListBox();
-            this.btnDelete = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnDeleteEquip = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // listBoxEquipment
+            // dataGridView1
             // 
-            this.listBoxEquipment.FormattingEnabled = true;
-            this.listBoxEquipment.Location = new System.Drawing.Point(217, 91);
-            this.listBoxEquipment.Name = "listBoxEquipment";
-            this.listBoxEquipment.Size = new System.Drawing.Size(120, 95);
-            this.listBoxEquipment.TabIndex = 0;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(108, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(473, 226);
+            this.dataGridView1.TabIndex = 0;
             // 
-            // btnDelete
+            // btnDeleteEquip
             // 
-            this.btnDelete.Location = new System.Drawing.Point(238, 224);
-            this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
-            this.btnDelete.TabIndex = 1;
-            this.btnDelete.Text = "Delete";
-            this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            this.btnDeleteEquip.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnDeleteEquip.Font = new System.Drawing.Font("Goudy Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeleteEquip.Location = new System.Drawing.Point(256, 281);
+            this.btnDeleteEquip.Name = "btnDeleteEquip";
+            this.btnDeleteEquip.Size = new System.Drawing.Size(185, 49);
+            this.btnDeleteEquip.TabIndex = 9;
+            this.btnDeleteEquip.Text = "Delete Equipment";
+            this.btnDeleteEquip.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 17);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Please select the data you want to delete :";
             // 
             // frmDelEquip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CAS_Engineering_Main.Properties.Resources._6;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.listBoxEquipment);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDeleteEquip);
+            this.Controls.Add(this.dataGridView1);
             this.DoubleBuffered = true;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmDelEquip";
             this.Text = "frmDelEquip";
             this.Load += new System.EventHandler(this.frmDelEquip_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBoxEquipment;
-        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnDeleteEquip;
+        private System.Windows.Forms.Label label1;
     }
 }
