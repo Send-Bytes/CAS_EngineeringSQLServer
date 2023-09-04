cbd866a22d09cdfe6818bdc3234f97649c39f7a5
ZaneKennard <142911294+ZaneKennard@users.noreply.github.com>
2023-09-04 17:05:09 +0200

    allocate

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmAllocateEmps.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmAllocateEmps.Designer.cs
@@ -28,19 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbEmp = new System.Windows.Forms.ComboBox();
+            this.cmbProjId = new System.Windows.Forms.ComboBox();
+            this.txtRole = new System.Windows.Forms.TextBox();
+            this.btnAllocate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // cmbEmp
+            // 
+            this.cmbEmp.FormattingEnabled = true;
+            this.cmbEmp.Location = new System.Drawing.Point(533, 195);
+            this.cmbEmp.Name = "cmbEmp";
+            this.cmbEmp.Size = new System.Drawing.Size(121, 21);
+            this.cmbEmp.TabIndex = 0;
+            // 
+            // cmbProjId
+            // 
+            this.cmbProjId.FormattingEnabled = true;
+            this.cmbProjId.Location = new System.Drawing.Point(373, 195);
+            this.cmbProjId.Name = "cmbProjId";
+            this.cmbProjId.Size = new System.Drawing.Size(121, 21);
+            this.cmbProjId.TabIndex = 1;
+            // 
+            // txtRole
+            // 
+            this.txtRole.Location = new System.Drawing.Point(526, 271);
+            this.txtRole.Name = "txtRole";
+            this.txtRole.Size = new System.Drawing.Size(128, 20);
+            this.txtRole.TabIndex = 2;
+            // 
+            // btnAllocate
+            // 
+            this.btnAllocate.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnAllocate.Font = new System.Drawing.Font("Goudy Old Style", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllocate.Location = new System.Drawing.Point(421, 313);
+            this.btnAllocate.Name = "btnAllocate";
+            this.btnAllocate.Size = new System.Drawing.Size(185, 49);
+            this.btnAllocate.TabIndex = 10;
+            this.btnAllocate.Text = "Allocate Employee";
+            this.btnAllocate.UseVisualStyleBackColor = false;
+            this.btnAllocate.Click += new System.EventHandler(this.btnAllocate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(370, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Employee Role  :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(555, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Employees :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(399, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Projects :";
             // 
             // frmAllocateEmps
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CAS_Engineering_Main.Properties.Resources._1_Flipped;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(720, 393);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAllocate);
+            this.Controls.Add(this.txtRole);
+            this.Controls.Add(this.cmbProjId);
+            this.Controls.Add(this.cmbEmp);
+            this.DoubleBuffered = true;
             this.Name = "frmAllocateEmps";
             this.Text = "frmAllocateEmps";
+            this.Load += new System.EventHandler(this.frmAllocateEmps_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbEmp;
+        private System.Windows.Forms.ComboBox cmbProjId;
+        private System.Windows.Forms.TextBox txtRole;
+        private System.Windows.Forms.Button btnAllocate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmAllocateEmps.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmAllocateEmps.cs
@@ -7,14 +7,140 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CAS_Engineering_Main.Dash
 {
     public partial class frmAllocateEmps : Form
     {
+        Form1 frm1;
+        string conStr;
+
         public frmAllocateEmps()
         {
             InitializeComponent();
+            frm1 = new Form1();
+        }
+
+        private void frmAllocateEmps_Load(object sender, EventArgs e)
+        {
+            try
+            {
+
+                conStr = frm1.conString;
+
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    string sql = "SELECT P_Description FROM Projects";
+
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbProjId.Items.Clear();
+                        cmbProjId.Items.Add(reader.GetValue(0));
+                    }
+
+                    reader.Close();
+
+                    sql = "SELECT FirstName, LastName FROM Clients";
+                    cmd = new SqlCommand(sql, con);
+
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbEmp.Items.Clear();
+                        string fName = reader.GetValue(0).ToString();
+                        string lName = reader.GetValue(1).ToString();
+                        cmbEmp.Items.Add(fName + " " + lName);
+                    }
+
+                    con.Close();
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
+        }
+
+
+        private void btnAllocate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conStr = frm1.conString;
+
+
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    con.Open();
+
+                    string sql = "SELECT Project_ID, P_Description FROM Projects";
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    int projId = 0;
+                    while (reader.Read())
+                    {
+                        if (cmbProjId.Text == reader.GetValue(1).ToString())
+                        {
+                            projId = Convert.ToInt32(reader.GetValue(0));
+                        }
+                    }
+
+                    reader.Close();
+
+                    sql = "SELECT Client_ID, FirstName FROM Client";
+                    string name = string.Empty;
+
+                    for (int i = 0; i < cmbEmp.Text.Length; i++)
+                    {
+                        if (cmbEmp.Text == " ")
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            name = name + cmbEmp.Text[i];
+                        }
+                    }
+
+                    cmd = new SqlCommand(sql, con);
+                    reader = cmd.ExecuteReader();
+                    int clientId = 0;
+
+                    while (reader.Read())
+                    {
+                        if (name == reader.GetValue(1).ToString())
+                        {
+                            clientId = Convert.ToInt32(reader.GetValue(0));
+                        }
+                    }
+
+                    reader.Close();
+
+                    sql = $"INSERT INTO Project_Emp (Project_ID, Employee_ID, Employee_Role) " +
+                        $"VALUES ({projId}, {clientId}, '{txtRole.Text}')";
+
+                    cmd = new SqlCommand(sql, con);
+                    SqlDataAdapter adap = new SqlDataAdapter();
+                    adap.InsertCommand = cmd;
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
         }
     }
 }
