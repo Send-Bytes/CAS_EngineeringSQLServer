ae0cc0304f0bf06d1e1a4ce71bbd113f0bbe02f2
ZaneKennard <142911294+ZaneKennard@users.noreply.github.com>
2023-09-04 17:31:16 +0200

    Project Equipment

--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmProjEquip.Designer.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmProjEquip.Designer.cs
@@ -28,19 +28,114 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cmbProjId = new System.Windows.Forms.ComboBox();
+            this.cmbEquip = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cmbProjId
+            // 
+            this.cmbProjId.FormattingEnabled = true;
+            this.cmbProjId.Location = new System.Drawing.Point(291, 182);
+            this.cmbProjId.Name = "cmbProjId";
+            this.cmbProjId.Size = new System.Drawing.Size(121, 21);
+            this.cmbProjId.TabIndex = 2;
+            // 
+            // cmbEquip
+            // 
+            this.cmbEquip.FormattingEnabled = true;
+            this.cmbEquip.Location = new System.Drawing.Point(442, 182);
+            this.cmbEquip.Name = "cmbEquip";
+            this.cmbEquip.Size = new System.Drawing.Size(121, 21);
+            this.cmbEquip.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Goldenrod;
+            this.button1.Font = new System.Drawing.Font("Goudy Old Style", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(334, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 49);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Allocate Equipment";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(443, 237);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(288, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Quantity recieved :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(462, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Equipment :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(319, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Projects :";
             // 
             // frmProjEquip
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(790, 419);
+            this.BackgroundImage = global::CAS_Engineering_Main.Properties.Resources._1_Flipped;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(598, 354);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbEquip);
+            this.Controls.Add(this.cmbProjId);
+            this.DoubleBuffered = true;
             this.Name = "frmProjEquip";
             this.Text = "frmProjEquip";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.ComboBox cmbProjId;
+        private System.Windows.Forms.ComboBox cmbEquip;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmProjEquip.cs
+++ CAS_Engineering_Main/CAS_Engineering_Main/Dash/frmProjEquip.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CAS_Engineering_Main.Dash
 {
     public partial class frmProjEquip : Form
     {
+        Form1 frm1;
+        string conStr;
         public frmProjEquip()
         {
             InitializeComponent();
+            frm1 = new Form1();
+        }
+
+        private void frmProjEquip_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string conStr = frm1.conString;
+
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    string sql = "SELECT P_Description FROM Projects";
+
+                    con.Open();
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbProjId.Items.Clear();
+                        cmbProjId.Items.Add(reader.GetValue(0));
+                    }
+
+                    reader.Close();
+
+                    sql = "SELECT E_Description FROM Equipment";
+                    cmd = new SqlCommand(sql, con);
+
+                    reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        cmbEquip.Items.Clear();
+                        cmbEquip.Items.Add(reader.GetValue(0));
+                    }
+
+                    con.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                conStr = frm1.conString;
+
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    con.Open();
+
+                    string sql = "SELECT Project_ID, P_Description FROM Projects";
+
+                    SqlCommand cmd = new SqlCommand(sql, con);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    int projId = 0;
+                    while (reader.Read())
+                    {
+                        if (cmbProjId.Text == reader.GetValue(1).ToString())
+                        {
+                            projId = Convert.ToInt32(reader.GetValue(0));
+                        }
+                    }
+
+                    reader.Close();
+
+                    sql = "SELECT E_ID, E_Description FROM Equipment";
+
+                    cmd = new SqlCommand(sql, con);
+                    reader = cmd.ExecuteReader();
+                    int eId = 0;
+
+                    while (reader.Read())
+                    {
+                        if (cmbEquip.Text == reader.GetValue(1).ToString())
+                        {
+                            eId = Convert.ToInt32(reader.GetValue(0));
+                        }
+                    }
+
+                    int qty = Convert.ToInt32(numericUpDown1.Value);
+
+                    sql = $"INSERT INTO Project_Equip (Project_ID, Equipment_ID, Quantity_Received) " +
+                        $"VALUES ({projId}, {eId}, {qty})";
+
+                    cmd = new SqlCommand(sql, con);
+                    SqlDataAdapter adap = new SqlDataAdapter();
+
+                    adap.InsertCommand = cmd;
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("An error occured: " + ex.Message);
+            }
+
+
+
         }
     }
 }
